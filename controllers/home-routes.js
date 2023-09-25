db1453aaf2562783b4141e93829b2f296d4b6977
Donna Goon <g.donna120@gmail.com>
2023-09-25 06:33:56 -0500

    cleaned-up-handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {Blogposts, User} = require('../models');
 
 // GET all galleries for homepage
-router.get('/gallery/:id', async (req, res) => {
+router.get('/Blogposts/:id', async (req, res) => {
   const dbblogData = await Blogposts.findAll({
     include: [
       {
@@ -14,25 +14,5 @@ router.get('/gallery/:id', async (req, res) => {
   },
   );
   
-
-
-// GET one painting
-router.get('/painting/:id', async (req, res) => {
-  try {
-    const dbPaintingData = await Painting.findByPk(req.params.id);
-
-    const painting = dbPaintingData.get({ plain: true });
-
-    res.render('painting', {
-      painting,
-      // We are not incrementing the 'countVisit' session variable here
-      // but simply sending over the current 'countVisit' session variable to be rendered
-      countVisit: req.session.countVisit,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
   
 module.exports = router;
